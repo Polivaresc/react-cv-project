5451bdf0963b9cc2708b4bc4256a6735e405cefd
Polivaresc <paulaolca98@gmail.com>
2022-03-08 11:17:07 +0100

    working on form validation...

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,7 +7,8 @@ class Education extends Component {
 
         this.state = {
             showForm: false,
-            educationList: []
+            educationList: [],
+            validForm: false
         }
 
         this.showEducationForm = this.showEducationForm.bind(this)
@@ -15,17 +16,31 @@ class Education extends Component {
         this.deleteEducation = this.deleteEducation.bind(this)
     }
 
-    showEducationForm() {
+    showEducationForm(valid) {
+
         this.setState({
-            showForm: true
+            validForm: valid
         })
+
+        if(!this.state.valid) {
+            this.setState({
+                showForm: true
+            })
+        }
     }
 
-    addEducationToList(education){
+    addEducationToList(education, valid){
+
         this.setState({
-            educationList: this.state.educationList.concat(education),
-            showForm: false
+            validForm: valid
         })
+
+        if(this.state.validForm) {
+            this.setState({
+                educationList: this.state.educationList.concat(education),
+                showForm: false
+            })
+        }
     }
 
     deleteEducation(e) {
@@ -39,7 +54,7 @@ class Education extends Component {
             <div>
                 <h3>Education:</h3>
                 <button className="add-button" onClick={this.showEducationForm}>Add Education +</button>
-                {this.state.showForm && <EducationForm addEducationToList={this.addEducationToList}/>}
+                {this.state.showForm && <EducationForm addEducationToList={this.addEducationToList} valid={this.isValid}/>}
                 <EducationList educationList={this.state.educationList} deleteEducation={this.deleteEducation}/>
             </div>
         )
@@ -57,10 +72,13 @@ class EducationForm extends Component {
                 date: '',
                 id: uniqid()
             },
+            invalid: false,
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.validateForm = this.validateForm.bind(this)
+        this.isValid = this.isValid.bind(this)
     }
 
     handleChange(e) {
@@ -71,6 +89,7 @@ class EducationForm extends Component {
 
     handleSubmit(e) {
         e.preventDefault()
+
         this.props.addEducationToList(this.state.education)
         this.setState({
             education: {
@@ -79,6 +98,36 @@ class EducationForm extends Component {
                 date: '',
                 id: uniqid()
         }})
+
+        if(this.validateForm()) {
+            this.setState({
+                invalid: true
+            })
+        }
+    }
+
+    validateForm() {
+        const errors = {}
+
+        if(!this.state.education.title) {
+            errors.title = 'This field is required'
+        } 
+
+        /* if(!e.target.email) {
+            errors.email = 'Your email is required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.email)) {
+            errors.email = 'Please, enter a valid email address'
+        } */
+
+        if(!this.state.education.institution) {
+            errors.institution = 'This field is required'
+        }
+
+        return errors
+    }
+
+    isValid() {
+        return !this.state.invalid
     }
 
     render() {
@@ -87,6 +136,7 @@ class EducationForm extends Component {
                 <div>
                     <label htmlFor="studies-title">Title:</label>
                     <input type="text" id="studies-title" name="title" onChange={this.handleChange}/>
+                    {!this.isValid() ? <Error validateForm={this.validateForm} name="title"/> : ''}
                 </div>
                 <div>
                     <label htmlFor="studies-institution">Institution:</label>
@@ -99,6 +149,15 @@ class EducationForm extends Component {
                 <button type="submit" className="submit-button" onClick={this.handleSubmit}>Done</button>
             </form>
     )}
+}
+
+const Error = (props) => {
+    const { validateForm, name } = props
+    const errors = validateForm()
+    const message = errors[name]
+    return(
+        <div>{message}</div>
+    )
 }
 
 const EducationList = (props) => {
